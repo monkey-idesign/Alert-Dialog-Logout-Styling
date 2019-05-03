ac070eaaca8a8f42891fe7f4b5e9579e9e8b3cb9
Samreaksa Ros <15318223+monkeyidesign@users.noreply.github.com>
2019-05-03 15:45:07 +0700

    Initial Commit

---
+++ Alert like logout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alert like logout
+//
+//  Created by Apple on 5/3/19.
+//  Copyright © 2019 Monkey iDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
